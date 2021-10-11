61b9f91d0dd61c62b99dd8aaf96dde5533d795b9
ankushmalode <you@example.com>
2021-10-11 14:09:48 +0530

    Creating TicTacToeGame board

--- src/main/java/cpm/bridgelabz/TicTacToeGame.java
+++ src/main/java/cpm/bridgelabz/TicTacToeGame.java
@@ -1,22 +1,19 @@
 package cpm.bridgelabz;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class TicTacToeGame {
     public static void main(String[] args) {
-//        Scanner in = new Scanner(System.in);
-        int row, column;
-        char player = 'X';
-        board();
-    }
-
-        //create 1 dimensional array for tic tac toe board
-    public static void board(){
-        char[] board = new char[10];
-//        char ch = '1';
-        for (int i = 1; i < board.length; i++) {
-            board[i] = ' ';
+        char[][] gameBoard={{' ','|',' ','|',' '},
+                {'-','+','-','+','-'},
+                {' ','|',' ','|',' '},
+                {'-','+','-','+','-'},
+                {' ','|',' ','|',' '}};
+        for (char[] row:gameBoard){
+            for (char c:row){
+                System.out.print(c);
+            }
+            System.out.println();
         }
     }
-
 }
